fe00edb6d070f8266888122dfd4deb17bd258e28
victory chrisnanto <vkrisna65@gmail.com>
2020-10-30 15:04:13 +0700

    modify cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -29,10 +29,15 @@ class CorsMiddleware
         }
 
         $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header->set($key, $value);
-        }
+        $response->headers->set('Access-Control-Allow-Origin' , '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Credentials' , 'true');
+        $response->headers->set('Access-Control-Max-Age' , '86400');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+        // foreach($headers as $key => $value)
+        // {
+        //     $response->header->set($key, $value);
+        // }
 
         return $response;
     }
